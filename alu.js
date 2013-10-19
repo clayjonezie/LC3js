aaa520b354fbe668b339c65bb79ab96ed30d73a9
Clay Jones <clay.mark.jones@gmail.com>
2013-10-18 17:58:34 -0700

    renamed arithmeticLogicUnit.js alu.js

---
+++ alu.js
@@ -0,0 +1,21 @@
+var alu = (function() {
+    var add = function(a, b) {
+        a.forEach(function(elem, i, arr) {
+                      
+        });
+    }
+
+    var and = function(a, b) {
+        var output = []; 
+        a.forEach(function(elem, i, arr) {
+            if (elem && (elem[i])) {
+                output[i] = true;
+            }
+        });
+    }
+
+    var not = function(a, b) {
+        var output = [];
+
+    }
+}());
